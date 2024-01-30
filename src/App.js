fc1f12103ccc44a3311578e7bfcc0426ac064bcb
Aman-993 <jangraaman993@gmail.com>
2024-01-31 00:51:36 +0530

    new code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+
 import './App.css';
+import HomeComponents from './Components/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomeComponents />
     </div>
   );
 }
 
 export default App;
+// Manifest
+// Service worker
+// https://
+// favicon.icon
+// maskable icon
+// APP name
+// install swr
+// npx create-react-app my-app --template cra-template-pwa
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.register();
+
+
+
+
+
+
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
